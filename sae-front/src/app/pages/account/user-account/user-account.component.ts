7a54e7aaf6ec29546e67497218a948cd73c7a7f7
Gabin Munoz <32036115+Munozmu@users.noreply.github.com>
2024-01-31 20:00:25 +0100

    implement profile improvements and add tabbing system

--- sae-front/src/app/pages/account/user-account/user-account.component.ts
+++ sae-front/src/app/pages/account/user-account/user-account.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BookCardSmallComponent } from '../../../components/shared/book-card-small/book-card-small.component';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-user-account',
   standalone: true,
-  imports: [BookCardSmallComponent],
+  imports: [BookCardSmallComponent, CommonModule, RouterModule],
   templateUrl: './user-account.component.html',
   styleUrl: './user-account.component.scss',
 })
-export class UserAccountComponent {}
+export class UserAccountComponent implements OnInit {
+
+  tabs = [
+    { title: 'Profil', link: 'profile', isSelected: true },
+    { title: 'Mes réservations', link: 'booking', isSelected: false },
+  ];
+
+  currentTab = 'profile';
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+
+    // Add the param to url by default
+    this.router.navigate(['/account'], { queryParams: { tab: 'profile' } });
+
+    // Get tab param in the url
+    this.route.queryParams.subscribe(params => {
+      this.currentTab = params['tab'];
+    });
+
+  }
+
+  changeTab(link: string): void {
+    // add param to url
+    this.router.navigate(['/account'], { queryParams: { tab: link } });
+  }
+
+
+
+}
