0c34a4fe775c23014273a62a1273cbc0fc225be1
Gabin Munoz <32036115+Munozmu@users.noreply.github.com>
2024-01-31 12:26:44 +0100

    fix login

--- sae-front/src/app/components/shared/navbar/navbar.component.ts
+++ sae-front/src/app/components/shared/navbar/navbar.component.ts
@@ -11,7 +11,7 @@ import { CommonModule } from '@angular/common';
 })
 export class NavbarComponent {
 
-  isLoginModalDisplayed: boolean = false;
+  isLoginModalDisplayed: boolean = true;
 
   onModalClosed(isClosed: boolean) {
     if (isClosed) this.isLoginModalDisplayed = false;
