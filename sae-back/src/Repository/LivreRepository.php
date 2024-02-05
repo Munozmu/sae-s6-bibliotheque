5978fc9da77564f852888802f68307b84b2aaea4
Guinle Loic <101970621+Guinlel@users.noreply.github.com>
2024-02-05 12:09:20 +0100

    Improve search , left to add author and category

--- sae-back/src/Repository/LivreRepository.php
+++ sae-back/src/Repository/LivreRepository.php
@@ -23,45 +23,81 @@ class LivreRepository extends ServiceEntityRepository
         parent::__construct($registry, Livre::class);
     }
 
-//    /**
-//     * @return Livre[] Returns an array of Livre objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('l')
-//            ->andWhere('l.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('l.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return Livre[] Returns an array of Livre objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('l')
+    //            ->andWhere('l.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('l.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
 
-    public function searchByParameter($keyword,$lang,$anneeMin,$anneeMax) : array
+    public function searchByParameter($keyword, $lang, $anneeMin, $anneeMax): array
     {
-    return $this->createQueryBuilder('l')
-        ->andWhere("l.titre LIKE :keyword")
-        ->andWhere("l.langue LIKE :lang")
-        ->andWhere("YEAR(l.date_sortie) BETWEEN :anneeMin AND :anneeMax")
-        ->setParameters([
+        // return $this->createQueryBuilder('l')
+        //     ->andWhere("l.titre LIKE :keyword")
+        //     ->andWhere("l.langue LIKE :lang")
+        //     ->andWhere("l.dateSortie BETWEEN :anneeMin AND :anneeMax")
+        //     ->setParameters([
+        //         'keyword' => '%' . $keyword . '%',
+        //         'lang' => '%' . $lang . '%',
+        //         'anneeMin' => $anneeMin . '-01-01',
+        //         'anneeMax' => $anneeMax . '-31-12'
+        //     ])
+        //     ->getQuery()
+        //     ->getResult();
+
+        // $entityManager = $this->getEntityManager();
+
+        // $query = $entityManager->createQuery(
+        //     "Select l
+        //     From App\Entity\Livre l
+        //     Where l.titre LIKE :keyword
+        //     And l.langue LIKE :lang
+        //     And l.dateSortie BETWEEN '2024-01-01' AND '2025-31-12'
+        //     "
+        // )->setParameters([
+        //     'keyword' => '%' . $keyword . '%',
+        //     'lang' => '%' . $lang . '%',
+        //     'anneeMin' => '2024-01-01',
+        //     'anneeMax' => '2025-31-12'
+        // ]);
+
+        // return $query->getResult();
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = "
+            SELECT *
+            FROM livre
+            WHERE livre.titre LIKE :keyword 
+            AND livre.date_sortie BETWEEN :anneeMin AND :anneeMax 
+            AND livre.langue LIKE :lang
+        ";
+
+        $resultSet = $conn->executeQuery($sql, [
             'keyword' => '%' . $keyword . '%',
             'lang' => '%' . $lang . '%',
-            'anneeMin' => $anneeMin,
-            'anneeMax' => $anneeMax
-        ])
-        ->getQuery()
-        ->getResult();
+            'anneeMin' => $anneeMin . '-01-01',
+            'anneeMax' => $anneeMax . '-12-31',
+        ]);
+
+        return $resultSet->fetchAllAssociative();
     }
 
-//    public function findOneBySomeField($value): ?Livre
-//    {
-//        return $this->createQueryBuilder('l')
-//            ->andWhere('l.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?Livre
+    //    {
+    //        return $this->createQueryBuilder('l')
+    //            ->andWhere('l.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
