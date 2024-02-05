e0d8204dfaf32a9ba5981517c05cb561095dc857
Ozouka <zouli.karim.pro@gmail.com>
2024-02-05 11:38:51 +0100

    Update small card

--- sae-front/src/app/components/shared/book-card-small/book-card-small.component.ts
+++ sae-front/src/app/components/shared/book-card-small/book-card-small.component.ts
@@ -1,16 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Book } from '../../../core/models/book';
-import { DatePipe } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-book-card-small',
   standalone: true,
-  imports: [DatePipe],
+  imports: [CommonModule, DatePipe],
   templateUrl: './book-card-small.component.html',
-  styleUrl: './book-card-small.component.scss'
+  styleUrl: './book-card-small.component.scss',
 })
 export class BookCardSmallComponent {
-
   @Input() book: Book = {} as Book;
-
+  @Input() isReserved: boolean = false;
 }
