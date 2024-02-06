407c0e2e18586d3538bae96599f4525139677f47
Gabin Munoz <32036115+Munozmu@users.noreply.github.com>
2024-02-06 10:17:04 +0100

    feat(book service): create method to check book valability

--- sae-front/src/app/core/services/book.service.ts
+++ sae-front/src/app/core/services/book.service.ts
@@ -33,4 +33,18 @@ export class BookService {
   }
 
 
+  /**
+   * Check if a book can be booked or not
+   * @param book Book to check
+   * @returns true if the book is avalaible
+   */
+  isBookAvailable(book: Book): boolean {
+    const isBookBorrowed = book.emprunts && book.emprunts.length > 0 && book.emprunts[book.emprunts.length - 1].enCours;
+    const isBookReserved = book.reservations && book.reservations.length > 0;
+
+    return !(isBookBorrowed || isBookReserved);
+  }
+
+
+
 }
