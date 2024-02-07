f958ded22570af91603e87002b2df9a07b9eb0e3
Gabin Munoz <32036115+Munozmu@users.noreply.github.com>
2024-02-07 22:30:41 +0100

    Revert "Add reservation card for books"

--- sae-front/src/app/components/shared/book-card-small/book-card-small.component.ts
+++ sae-front/src/app/components/shared/book-card-small/book-card-small.component.ts
@@ -1,16 +1,16 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Book } from '../../../core/models/book';
-import { CommonModule, DatePipe } from '@angular/common';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-book-card-small',
   standalone: true,
-  imports: [CommonModule, DatePipe],
+  imports: [DatePipe],
   templateUrl: './book-card-small.component.html',
-  styleUrl: './book-card-small.component.scss',
+  styleUrl: './book-card-small.component.scss'
 })
 export class BookCardSmallComponent {
+
   @Input() book: Book = {} as Book;
-  @Input() isReserved: boolean = false;
-  @Input() isDisabled: boolean = false;
+
 }
