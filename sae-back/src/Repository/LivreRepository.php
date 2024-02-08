5590c76b11acb56ef9d15b3c5d99a26a19243a40
Guinle Loic <101970621+Guinlel@users.noreply.github.com>
2024-02-08 11:00:12 +0100

    Search is back again

--- sae-back/src/Repository/LivreRepository.php
+++ sae-back/src/Repository/LivreRepository.php
@@ -56,11 +56,11 @@ class LivreRepository extends ServiceEntityRepository
     public function getLivresNonEmpruntes(): array
     {
         return $this->createQueryBuilder('livre')
-        ->leftJoin('livre.emprunts', 'emprunt')
-        ->andWhere('emprunt.enCours = false OR emprunt.id IS NULL')
-        ->select('livre.titre', 'livre.id')
-        ->getQuery()
-        ->getResult();
+            ->leftJoin('livre.emprunts', 'emprunt')
+            ->andWhere('emprunt.enCours = false OR emprunt.id IS NULL')
+            ->select('livre.titre', 'livre.id')
+            ->getQuery()
+            ->getResult();
     }
 
     // public function getLivresNonDisponiblesAvecDateRetour(): array
@@ -72,7 +72,37 @@ class LivreRepository extends ServiceEntityRepository
     //         ->getResult();
     // }
 
-//    /**
+    public function searchByParameter($keyword, $lang, $anneeMin, $anneeMax, $author, $category): array
+    {
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = "
+            SELECT livre.id AS livre_id ,livre.titre,livre.date_sortie,livre.langue,livre.photo_couverture,livre.resume,auteur.id AS auteur_id,auteur.nom AS auteur_nom , auteur.prenom , categorie.id AS cate_id , categorie.nom AS cate_nom
+            FROM livre , auteur , auteur_livre , livre_categorie, categorie
+            WHERE auteur_livre.livre_id = livre.id
+            AND auteur_livre.auteur_id = auteur.id
+            AND livre_categorie.livre_id = livre.id
+            AND livre_categorie.categorie_id = categorie.id
+            AND livre.titre LIKE :keyword 
+            AND livre.date_sortie BETWEEN :anneeMin AND :anneeMax 
+            AND livre.langue LIKE :lang
+            AND auteur.nom LIKE :auteur
+            AND categorie.nom LIKE :categorie
+        ";
+
+        $resultSet = $conn->executeQuery($sql, [
+            'keyword' => '%' . $keyword . '%',
+            'lang' => '%' . $lang . '%',
+            'anneeMin' => $anneeMin . '-01-01',
+            'anneeMax' => $anneeMax . '-12-31',
+            'auteur' => '%' . $author . '%',
+            'categorie' => '%' . $category . '%'
+        ]);
+
+        return $resultSet->fetchAllAssociative();
+    }
+
+    //    /**
     //     * @return Livre[] Returns an array of Livre objects
     //     */
     //    public function findByExampleField($value): array
@@ -87,13 +117,13 @@ class LivreRepository extends ServiceEntityRepository
     //        ;
     //    }
 
-//    public function findOneBySomeField($value): ?Livre
-//    {
-//        return $this->createQueryBuilder('l')
-//            ->andWhere('l.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?Livre
+    //    {
+    //        return $this->createQueryBuilder('l')
+    //            ->andWhere('l.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
