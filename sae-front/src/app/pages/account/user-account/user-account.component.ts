14933c040ae628893edb0218b98f0c51d17f407a
Ozouka <zouli.karim.pro@gmail.com>
2024-02-08 17:57:47 +0100

    Ajout du Voir emprunt

--- sae-front/src/app/pages/account/user-account/user-account.component.ts
+++ sae-front/src/app/pages/account/user-account/user-account.component.ts
@@ -4,21 +4,44 @@ import { CommonModule, DatePipe } from '@angular/common';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { Adherent } from '../../../core/models/adherent';
 import { AuthService } from '../../../core/auth/auth.service';
-import { AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { AdherentService } from '../../../core/services/adherent.service';
 
 @Component({
   selector: 'app-user-account',
   standalone: true,
-  imports: [BookCardSmallComponent, CommonModule, RouterModule, DatePipe, FormsModule, ReactiveFormsModule],
+  imports: [
+    BookCardSmallComponent,
+    CommonModule,
+    RouterModule,
+    DatePipe,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './user-account.component.html',
   styleUrl: './user-account.component.scss',
 })
 export class UserAccountComponent implements OnInit {
-
   tabs = [
     { title: 'Profil', link: 'profile', isSelected: true },
     { title: 'Mes réservations', link: 'booking', isSelected: false },
+    {
+      title: 'Mes emprunts effectuées',
+      link: 'bookingMade',
+      isSelected: false,
+    },
+    {
+      title: 'Voir emprunts',
+      link: 'handleEmprunt',
+      isSelected: false,
+    },
   ];
 
   currentTab = 'profile';
@@ -27,15 +50,13 @@ export class UserAccountComponent implements OnInit {
 
   confirmMessage = '';
 
-
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private authService: AuthService,
     private fb: FormBuilder,
     private adherentService: AdherentService
-  ) {
-  }
+  ) {}
 
   get formControlEditProfile(): { [key: string]: AbstractControl } {
     return this.editProfileForm.controls;
@@ -44,32 +65,31 @@ export class UserAccountComponent implements OnInit {
   currentUser$ = this.authService.currentUser$;
 
   ngOnInit(): void {
-
     // Add the param to url by default
     this.router.navigate(['/account'], { queryParams: { tab: 'profile' } });
 
     // Get tab param in the url
-    this.route.queryParams.subscribe(params => {
+    this.route.queryParams.subscribe((params) => {
       this.currentTab = params['tab'];
     });
 
     // Get user
-    this.currentUser$.subscribe(
-      (user) => {
-        if (user) {
-          this.editProfileForm = this.fb.group({
-            id: [user.id],
-            prenom: [user.prenom, [Validators.required]],
-            nom: [user.nom, [Validators.required]],
-            numTel: [user.numTel, [Validators.required, Validators.pattern(/^\d{10}$/)]],
-            dateNaiss: [user.dateNaiss, [Validators.required]],
-            adressePostale: [user.adressePostale, [Validators.required]],
-            email: [user.email, [Validators.required, Validators.email]],
-          });
-        }
+    this.currentUser$.subscribe((user) => {
+      if (user) {
+        this.editProfileForm = this.fb.group({
+          id: [user.id],
+          prenom: [user.prenom, [Validators.required]],
+          nom: [user.nom, [Validators.required]],
+          numTel: [
+            user.numTel,
+            [Validators.required, Validators.pattern(/^\d{10}$/)],
+          ],
+          dateNaiss: [user.dateNaiss, [Validators.required]],
+          adressePostale: [user.adressePostale, [Validators.required]],
+          email: [user.email, [Validators.required, Validators.email]],
+        });
       }
-    );
-
+    });
   }
 
   changeTab(link: string): void {
@@ -88,7 +108,4 @@ export class UserAccountComponent implements OnInit {
       }
     );
   }
-
-
-
 }
